fe7f4d730bf5aa5a9bf3d7fbb00aa7596b2a46e2
André van Delft <andrevandelft@outlook.com>
2023-09-02 00:08:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="heidegger_theme",
     version="1.0",
-    description="Heidegger translations",
+    description="Theme for our Heidegger sites",
     packages=find_packages(),
     include_package_data=True,
 )
