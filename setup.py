d889b7b440ebb06fa161b417f2dd0f05c06d684f
André van Delft <andrevandelft@outlook.com>
2023-09-01 23:38:04 +0200

    Wrap theme in python package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="heidegger_theme",
+    version="1.0",
+    description="Heidegger translations",
+    packages=find_packages(),
+    include_package_data=True,
+)
